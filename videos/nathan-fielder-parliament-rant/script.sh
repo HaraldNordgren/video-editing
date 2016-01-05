4cde546f31ed9dd55d71eeb5dd1fffb7a24f9954
HaraldNordgren <haraldnordgren@gmail.com>
2016-01-06 00:30:13 +0100

    parliament-hill done

--- videos/nathan-fielder-parliament-rant/script.sh
+++ videos/nathan-fielder-parliament-rant/script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-ffmpeg -i Nathan\'s\ Nook\ 1\ -\ Parliament\ Rant.mp4 -ss 12.26 -t 5.9 -c:v mpeg4 -c:a copy parliament-hill-ffmpeg.mp4 -y
+ffmpeg -i Nathan\'s\ Nook\ 1\ -\ Parliament\ Rant.mp4 -ss 12.26 -t 9 \
+    -c:v libx264 -c:a copy parliament-hill-ffmpeg.mp4 -y
